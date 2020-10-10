eb5d1396011b1e212a173a322f9142598b7518fa
seongbin <apple0192@gmail.com>
2020-10-10 11:19:49 +0800

    'db_ing...'

--- flask/app2.py
+++ flask/app2.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import flask_sqlalchemy
-from flask_marshmallow import marshmallow
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 import os
 
-app = Flask(__nama__)
+app = Flask(__name__)
 # path os, glob(regex) / pathlib Path
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-## db
+# db
 # db 위치 지정
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -19,7 +20,9 @@ db = SQLAlchemy(app)
 # marshmallow init SERIALIZATION -> JSON
 ma = Marshmallow(app)
 
+
 # db Model
+
 class Todos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     desc = db.Column(db.String(100), nullable=False)
@@ -27,26 +30,64 @@ class Todos(db.Model):
     def __init__(self, desc):
         self.desc = desc
 
+
 # Produnt schema
 class TodoSchema(ma.Schema):
     class Meta:
         fields = ('id', 'desc')
 
+
 # Schema init
-todo_schema = TodoSchema() # one to one
-todos_schema = TodoSchema(many = True) # select multiple rows
+todo_schema = TodoSchema()  # one to one
+todos_schema = TodoSchema(many=True)  # select multiple rows
 
 
-## RESTFUL API
+# RESTFUL API
 # Create todos
 @app.route('/todo', methods=['POST'])
 def add_todo():
     desc = request.json['desc']
     new_todo = Todos(desc)
-    
+
     db.session.add(new_todo)
     db.session.commit()
     return todo_schema.jsonify(new_todo)
+
+
+# get todos
+@app.route('/todo', methods=['GET'])
+def get_todos():
+    all_todos = Todos.query.all()
+    # python pickle
+    result = todos_schema.dump(all_todos)
+    return jsonify(result)
+
+
+# Get one todo
+@app.route('/todo/<id>', methods=['GET'])
+def get_todo(id):
+    todo = Todos.query.get(id)
+    return todo_schema.jsonify(todo)
+
+
+# Update
+@app.route('/todo/<id>', methods=['PUT'])
+def update_todo(id):
+    todo = Todos.query.get(id)
+    desc = request.json['desc']
+    todo.desc = desc
+    db.session.commit()
+    return todo_schema.jsoinfy(todo)
+
+
+# Delete
+@app.route('/todo/<id>', methods=['DELETE'])
+def delete_todo(id):
+    todo = Todos.query.get(id)
+    db.session.delete(todo)
+    db.session.commit()
+    return todo_schema.jsonify(todo)
+
 
 # RUN SERVER
 if __name__ == '__main__':
